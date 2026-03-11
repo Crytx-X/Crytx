210e26bc1d88a4841dcbdc5246e0c3922d28f37e
Crytx-X <dzgbalamking15@gmail.com>
2026-03-11 10:07:32 +0700

    FIXED

--- Library.lua
+++ Library.lua
@@ -2164,7 +2164,7 @@ local Misc = Window:Tab({Title = "Misc", Icon = "box"}) do
     })
 
     Misc:Toggle({
-        Title = "Enable Auto Gatlingrombak2",
+        Title = "Enable Auto Gatling",
         Value = Globals.AutoGatling, 
         Callback = function(state)
             Globals.AutoGatling = state
@@ -2296,7 +2296,7 @@ local Misc = Window:Tab({Title = "Misc", Icon = "box"}) do
                             
                             -- [!] BAGIAN YANG DIUBAH: Menambahkan offset posisi ke depan
                             -- Silakan ubah angka "2.5" di bawah ini. Makin besar angkanya, makin ke depan nembaknya.
-                            local jarakKedepan = 2 
+                            local jarakKedepan = 1.2 
                             
                             -- target.CFrame.LookVector adalah arah depan dari musuh
                             local posisiNembak = target.Position + (target.CFrame.LookVector * jarakKedepan)
