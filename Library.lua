92b12c4feee69325f6ce77d4eba6c59314a21914
Crytx-X <dzgbalamking15@gmail.com>
2026-03-11 09:34:19 +0700

    rt

--- Library.lua
+++ Library.lua
@@ -2164,7 +2164,7 @@ local Misc = Window:Tab({Title = "Misc", Icon = "box"}) do
     })
 
     Misc:Toggle({
-        Title = "Enable Auto Gatling",
+        Title = "Enable Auto Gatlingrt",
         Value = Globals.AutoGatling, 
         Callback = function(state)
             Globals.AutoGatling = state
