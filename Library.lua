bd3b5a5489bd7adf0c16be906454fb58e6ed2359
Crytx-X <dzgbalamking15@gmail.com>
2026-03-12 17:07:11 +0700

    s

--- Library.lua
+++ Library.lua
@@ -2493,7 +2493,7 @@ local Misc = Window:Tab({Title = "Misc", Icon = "box"}) do
     end
 
     Misc:Toggle({
-        Title = "Advanced Enemy Radar",
+        Title = "Advanced Enemy Radars",
         Desc = "Displays Upcoming Waves and Live Enemy Tracker",
         Value = Globals.EnemyTracker or false,
         Callback = function(v)
